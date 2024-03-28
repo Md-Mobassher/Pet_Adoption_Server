4ae9f745d7af135913a85765f0dd7a32dabb724a
Md Mobassher Hossain <mobassherpautex@gmail.com>
2024-03-28 18:16:27 +0600

    pet and adoption request added

---
+++ prisma/migrations/20240328121552_pet_and_adoption_request_model_added/migration.sql
@@ -0,0 +1,40 @@
+-- CreateEnum
+CREATE TYPE "adoptionStatus" AS ENUM ('PENDING', 'APPROVED', 'REJECTED');
+
+-- CreateTable
+CREATE TABLE "pets" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "species" TEXT NOT NULL,
+    "breed" TEXT NOT NULL,
+    "age" INTEGER NOT NULL,
+    "size" TEXT NOT NULL,
+    "location" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "temperament" TEXT NOT NULL,
+    "medicalHistory" TEXT NOT NULL,
+    "adoptionRequirements" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "pets_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "adoptionRequests" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "petId" TEXT NOT NULL,
+    "status" "adoptionStatus" NOT NULL DEFAULT 'PENDING',
+    "petOwnershipExperience" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "adoptionRequests_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "adoptionRequests" ADD CONSTRAINT "adoptionRequests_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "adoptionRequests" ADD CONSTRAINT "adoptionRequests_petId_fkey" FOREIGN KEY ("petId") REFERENCES "pets"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
